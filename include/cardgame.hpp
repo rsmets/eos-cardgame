446cd059261d77010137799ff162bfbadf3c253f
Ray Smets <rayjsmets@gmail.com>
2020-06-03 00:36:48 -0700

    init. got the table working add .

---
+++ include/cardgame.hpp
@@ -0,0 +1,46 @@
+#include <eosio/eosio.hpp>
+
+using namespace std;
+using namespace eosio;
+
+CONTRACT cardgame : public contract {
+  public:
+    using contract::contract;
+    cardgame(name receiver, name code, datastream<const char*> ds):contract(receiver, code, ds),
+    _usersT(receiver, receiver.value),
+    _games(receiver, receiver.value)
+    {}
+
+    ACTION trymessage(name sender, name recipient);
+    ACTION hi(name from, string text);
+
+  private:
+    TABLE user_info {
+      name    username;
+      string  text;
+      int win_count = 0;
+      int lost_count = 0;
+      
+      auto primary_key() const { return username.value; };
+    };
+    typedef multi_index<name("users"), user_info> users_table;
+    // users is what this table will be called on chain
+    // user_info is referring to what pattern this will follow
+    // users_table is the name of the type we just created
+
+    TABLE game_info {
+      name    gameId;
+      string  game_name;
+      bool isenabled;
+      
+      auto primary_key() const { return gameId.value; };
+    };
+    typedef multi_index<name("games"), game_info> games_table;
+    // users is what this table will be called on chain
+    // user_info is referring to what pattern this will follow
+    // users_table is the name of the type we just created
+
+
+    users_table _usersT;
+    games_table _games;
+};
---
+++ src/cardgame.cpp
@@ -0,0 +1,35 @@
+#include <cardgame.hpp>
+#include "gameplay.cpp"
+
+ACTION cardgame::trymessage(name sender, name recipient) {
+  //require_auth(get_self());
+  require_auth(sender);
+  print ("Sorry, you will have to say hello to, ", name{recipient}, " in person");
+}
+
+ACTION cardgame::hi(name user, string message) {
+
+  // Init the _message table
+  //users_table _usersT(get_self(), get_self().value);
+
+  // Find the record from _messages table
+  auto msg_itr = _usersT.find(user.value);
+  if (msg_itr == _usersT.end()) {
+    // Create a message record if it does not exist
+    _usersT.emplace(user, [&](auto& msg) {
+      msg.username = user;
+      msg.text = message;
+    });
+  } else {
+    // Modify a message record if it exists
+    _usersT.modify(msg_itr, user, [&](auto& msg) {
+      msg.text = message;
+    });
+  }
+
+  //require_auth(get_self()); // this should be uncommented for the tutorial to pick up!
+  //require_auth(user);
+  print ("hello, ", name{user});
+}
+
+EOSIO_DISPATCH(cardgame, (hi)(trymessage))
