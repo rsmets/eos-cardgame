22fa0cd3de4c5263b676ca1365100d286c559431
Ray Smets <rayjsmets@gmail.com>
2020-06-05 00:03:03 -0700

    more game info.

--- include/cardgame.hpp
+++ include/cardgame.hpp
@@ -18,11 +18,68 @@ CONTRACT cardgame : public contract {
     ACTION login(name user);
 
   private:
+
+    enum card_type: uint8_t {
+      EMTPY = 0,
+      FIRE = 1,
+      WOOD = 2,
+      WATER = 3,
+      NEUTRAL = 4,
+      VOID = 5
+    };
+
+    struct card {
+      uint8_t type;
+      uint8_t attack_point;
+    };
+
+    const map<uint8_t, card> card_dict = {
+      {0, {EMPTY, 1} },
+      {1, {FIRE, 1} },
+      {2, {FIRE, 1} },
+      {3, {FIRE, 2} },
+      {4, {FIRE, 2} },
+      {5, {FIRE, 3} },
+      {6, {WOOD, 1} },
+      {7, {WOOD, 1} },
+      {8, {WOOD, 2} },
+      {9, {WOOD, 2} },
+      {10, {WOOD, 3} },
+      {11, {WATER, 1} },
+      {12, {WATER, 1} },
+      {13, {WATER, 2} },
+      {14, {WATER, 2} },
+      {15, {WATER, 3} },
+      {16, {NEUTRAL, 3} },
+      {17, {VOID, 0} }
+    };
+    
+    enum game_status: int8_t {
+      ONGOING = 0,
+      PLAYER_WON = 1,
+      PLAYER_LOST = -1
+    };
+
+    struct game {
+      int8_t life_player = 5;
+      int8_t life_ai = 5;
+      vector<uint8_t> deck_player = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17}; // vector is just an unbounded (size) array
+      vector<uint8_t> ai_player = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17}; // vector is just an unbounded (size) array
+      vector<uint8_t> hand_player = {0,0,0,0};
+      vector<uint8_t> hand_ai = {0,0,0,0};
+      uint8_t selected_card_player = 0;
+      uint8_t selected_card_ai = 0;
+      int8_t status = ONGOING;
+      uint8_t life_lost_player = 0;
+      uint8_t life_lost_ai = 0;
+    };
+
     TABLE user_info {
       name    username;
       //string  text;
       uint64_t win_count = 1;
       uint64_t lost_count = 2;
+      game game_data;
       
       auto primary_key() const { return username.value; };
     };
